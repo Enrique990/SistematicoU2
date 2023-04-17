4378820a5e702957bf99bbe6295b3df20a43801f
Enrique <enrique.taleno16055@est.edu.ni>
2023-04-17 08:15:21 -0600

    funcion angulos

--- funciones.cpp
+++ funciones.cpp
@@ -1,3 +1,31 @@
 #include<iostream>
 
 using namespace std;
+
+void angulos();
+void triangulos();
+void area();
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    cout<<"poner el angulo ";
+    cin>>num;
+    return 0;
+}
+
+void angulos(int num){
+    if (int i > 0; i< 90)
+    {
+        cout<<"el angulo es agudo " <<endl;
+    }if (int i=90)
+    {
+        cout<<"el angulo es recto " <<endl;
+    }if (int i > 90; i< 180)
+    {
+        cout<<"el angulo es obtuso " <<endl;
+    }if (int i=180)
+    {
+        cout<<"el angulo es extendido " <<endl;
+    }
+}
