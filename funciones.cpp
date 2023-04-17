fa17d14e6dfc7be577b6c0293fb291c3feb77f4e
Enrique <enrique.taleno16055@est.edu.ni>
2023-04-17 08:42:26 -0600

    ultimo intento

--- funciones.cpp
+++ funciones.cpp
@@ -6,13 +6,37 @@ void angulos();
 void triangulos();
 void area();
 
+int largo, ancho, ar;
+
 int main(int argc, char const *argv[])
 {
+    int opcion =0;
+    while (opcion !=4)
+    {
+        cout << "1. Identificar la clasificacion de un ángulo segun su medida." << endl;
+        cout << "2. Identifica que tipo de clasificacion es un triángulo segun la medida de sus lados." << endl;
+        cout << "3. Calcular area." << endl;
+        cout << "4. Salir." << endl;
+        cin >> opcion;
+    }
     
+    switch (opcion)
+    {
+    case 1{
+        cout << "Ingrese el angulo: ";
+        cin >> num;
+        void angulos();
+        break;
+    }
+        
+    
+    default:
+        break;
+    }
     return 0;
 }
 
-void angulos(int num){
+void angulos(){
     if (int i > 0; i< 90)
     {
         cout<<"el angulo es agudo " <<endl;
@@ -39,4 +63,13 @@ void triangulos(){
     {
         cout<<"el triangulo es Escaleno " <<endl;
     }
+}
+
+void area(){
+    cout<<"ingrese el largo ";
+    cin>> largo;
+    cout<<"ingrese el ancho ";
+    cin>> ancho;
+    largo * ancho = ar;
+    cout<<"el area es: "<< ar <<endl;
 }
